3562831b1e166e7580a670709c9dfc702302ffaa
Daniel Horn <danielhorn101@gmail.com>
2020-07-15 15:31:18 +0200

    Button

--- SFML-Tutorial/MainMenuState.cpp
+++ SFML-Tutorial/MainMenuState.cpp
@@ -18,17 +18,29 @@ void MainMenuState::initKeybinds()
 
 }
 
+void MainMenuState::initFonts()
+{
+	if (!this->font.loadFromFile("Fonts/Dosis-Light.ttf"))
+		throw("ERROR::MAINMENUSTATE::COULD NOT LOAD FONT");
+}
+
 MainMenuState::MainMenuState(sf::RenderWindow* pWindow, std::map<std::string, int>* pSupportedKeys)
 	: State(pWindow, pSupportedKeys)
 {
+	this->initFonts();
 	this->initKeybinds();
+
+	this->gameStateButton = new Button(100.f, 100.f, 150.f, 50.f,
+		&this->font, "New Game",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200)
+		);
 	background.setSize(sf::Vector2f(pWindow->getSize().x, pWindow->getSize().y));
 	background.setFillColor(sf::Color::Blue);
 }
 
 MainMenuState::~MainMenuState()
 {
-
+	delete this->gameStateButton;
 }
 
 void MainMenuState::updateInput(const float& dt)
@@ -41,6 +53,8 @@ void MainMenuState::update(const float& dt)
 	this->updateMousePositions();
 	this->updateInput(dt);
 	
+	this->gameStateButton->update(this->mousePosView);
+
 	system("cls");
 	std::cout << this->mousePosView.x << " " << this->mousePosView.y << "\n";
 }
@@ -50,4 +64,6 @@ void MainMenuState::render(sf::RenderTarget* pTarget)
 	if (!pTarget)
 		pTarget = this->pWindow;
 	pTarget->draw(background);
+
+	this->gameStateButton->render(pTarget);
 }
--- SFML-Tutorial/MainMenuState.h
+++ SFML-Tutorial/MainMenuState.h
@@ -1,20 +1,26 @@
 #pragma once
 #include "State.h"
+#include "Button.h"
+
 class MainMenuState :
     public State
 {
 private:
     sf::RectangleShape background;
+    sf::Font font;
+
+    Button* gameStateButton;
 
     //Functions
-    void initKeyinds();
+    void initFonts();
+    void initKeybinds();
 public:
     //Constructors /Desturctors
     MainMenuState(sf::RenderWindow* pWindow, std::map<std::string, int>* pSupportedKeys);
     ~MainMenuState();
 
     //Functions
-    void initKeybinds();
+    
     void updateInput(const float& dt);
     void update(const float& dt);
     void render(sf::RenderTarget* pTarget);
